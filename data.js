524cede9414fb4d4e195592d159cc92d883245bb
sshukla1250 <sshukla1250@gmail.com>
2023-09-29 23:15:47 +0530

    add data.js file and file.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[{name:'Sandeep Shukla',email:'sandeep@test.com',mobile:'93848484884'},
+{name:'Vijay',email:'vijay@test.com',mobile:'938484333284'},
+{name:'Ravi',email:'ravi@test.com',mobile:'847737484884'},
+{name:'Yush',email:'yush@test.com',mobile:'93737784884'}
+];
+module.exports=data;
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const http=require('http');
+const data=require('./data');
 http.createServer((req,res)=>{
-    res.write('<h1>Welcome Nodejs!</h1>');
+    res.writeHead(200,{'Content-Type':'application/json'});
+    res.write(JSON.stringify(data));
     res.end();
     
 }).listen(4500);
 console.log('Server Started!');
+console.log('Server logs');
