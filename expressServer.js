c8d2628bed1f0097f4468c8902bdcde6b49ed17a
sshukla1250 <sshukla1250@gmail.com>
2023-10-03 21:51:18 +0530

    add expressServer.js

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,20 @@
 const express=require('express');
+const data=require('./data.js');
 const app=express();
 app.get('',(req,res)=>{
+    console.log('Data sent by Browser '+req.query.name);
     res.send('This is Home page!');
 });
 app.get('/about',(req,res)=>{
-    res.send('This is About page!');
+    
+
+    res.send(
+        '<h1>Hello world</h1> <a href="/help">go to Help page</a>'
+    );
 });
 app.get('/help',(req,res)=>{
-    res.send('This is Help page!');
+    
+    res.send('This is Help page!'+JSON.stringify(data));
 });
+
 app.listen(5000);
